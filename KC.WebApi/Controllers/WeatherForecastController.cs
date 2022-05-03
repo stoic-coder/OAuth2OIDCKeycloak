c556ba1eba3868153a383b20bbdaaf39855d38a8
Nisha Hans <hans@soft-park.de>
2022-05-03 19:05:45 +0200

    Removed custom checkPolicy method

--- KC.WebApi/Controllers/WeatherForecastController.cs
+++ KC.WebApi/Controllers/WeatherForecastController.cs
@@ -12,38 +12,26 @@ public class WeatherForecastController : ControllerBase
    
 
     private readonly ILogger<WeatherForecastController> _logger;
-    private readonly IAuthorizationService _authorizationService;
+   
     private IWeatherForecastRepository WeatherForecastRepository { get; }
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger,IConfiguration configuration,IWeatherForecastRepository weatherForecastRepository, IAuthorizationService authorizationService)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,IWeatherForecastRepository weatherForecastRepository)
     {
-        IsAuthEnabled = configuration.GetValue("IsAuthEnabled",true);
         _logger = logger;
-        _authorizationService = authorizationService;
+      
         WeatherForecastRepository = weatherForecastRepository;
     }
-
-    public bool IsAuthEnabled { get; set; }
-
-    //[Authorize(Policy = Policies.NeedsWeatherForecast)]
+ 
+    [Authorize(Policy = Policies.NeedsWeatherForecast)]
     [HttpGet("GetWeatherForecast", Name = nameof(GetWeatherForecast))]  
     [Produces("application/json")]
-    public async Task<IActionResult> GetWeatherForecast()
+    public  IActionResult GetWeatherForecast()
     {
-        if (IsAuthEnabled)
-        {
-            var isAuthorized = await CheckPolicy(User, Policies.NeedsWeatherForecast);
-            if (!isAuthorized) return Forbid();
-        }
         var remoteIp = HttpContext.Connection.RemoteIpAddress;
         var route = HttpContext.Request.Path;
         _logger.LogInformation($"Accessing {route} from ip {remoteIp}");
         var result =  WeatherForecastRepository.GetForecast(DateTime.Now);
         return Ok(result);
     }
-    private async Task<bool> CheckPolicy(ClaimsPrincipal user, string policyName)
-    {
-        var allowed = await _authorizationService.AuthorizeAsync(user, policyName);
-        return allowed is {Succeeded: true};
-    }
+    
 }
