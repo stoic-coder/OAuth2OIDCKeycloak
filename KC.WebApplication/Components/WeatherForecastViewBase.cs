d57e5952c82a91f8b0dafaea3ff5e9d783c0ac5a
Ralf Reddemann <stoiccoder@outlook.com>
2022-05-04 09:25:06 +0200

    Some Fixes

--- KC.WebApplication/Components/WeatherForecastViewBase.cs
+++ KC.WebApplication/Components/WeatherForecastViewBase.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using KC.Library;
 using KC.WebApplication.Data;
 using Microsoft.AspNetCore.Components;
@@ -10,6 +11,10 @@ public class WeatherForecastViewBase : ComponentBase
 
 #pragma warning disable CS8618
     [Inject] private IWeatherForecastService ApiService { get; set; }
+
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
 #pragma warning restore CS8618
     protected WeatherForecastResponseModel WeatherForecastResponse { get; set; } = null!;
 
@@ -24,7 +29,20 @@ public class WeatherForecastViewBase : ComponentBase
     protected override async Task OnInitializedAsync()
     {
         WeatherForecastResponse=await ApiService.GetWeatherForecastAsync();
-     
+        
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            Claims = authState.User.Claims.ToList();
+            var preferredUserNameClaim = authState.User.FindFirst("preferred_username");
+            if (preferredUserNameClaim is not null)
+            {
+                PreferredUserName = preferredUserNameClaim.Value;
+            }
+
+            var claim = Claims.FirstOrDefault(c => c.Value == "ook-weather");
+            if (claim is not null)
+            {
+                HasOokWeatherClaim = true;
+            }
     }
 
     public bool HasOokWeatherClaim { get; set; }
