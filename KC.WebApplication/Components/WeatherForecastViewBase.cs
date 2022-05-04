cc22a33bfe1b0979b9387b5fbb7bd460d4cc9c1c
Ralf Reddemann <stoiccoder@outlook.com>
2022-05-04 09:06:57 +0200

    Added query for claim

--- KC.WebApplication/Components/WeatherForecastViewBase.cs
+++ KC.WebApplication/Components/WeatherForecastViewBase.cs
@@ -1,23 +1,34 @@
+using System.Security.Claims;
 using KC.Library;
 using KC.WebApplication.Data;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace KC.WebApplication.Components;
 
 public class WeatherForecastViewBase : ComponentBase
 {
-   
-#pragma warning disable CS8618
-    [Inject] private IWeatherForecastService WeatherForecastService { get; set; }
-   
-#pragma warning restore CS8618
-    
-#pragma warning disable CS8618
-    protected List<WeatherForecast>? Forecasts{ get; private set; }
-#pragma warning restore CS8618
+    [Inject] public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
+    private List<Claim>? Claims { get; set; }
+    [Inject] private IWeatherForecastService? WeatherForecastService { get; set; }
+
+    public string? PreferredUserName { get; set; } = "Unknown";
+
+    protected List<WeatherForecast>? Forecasts { get; private set; }
+
 
     protected override async Task OnInitializedAsync()
     {
-        Forecasts = await WeatherForecastService.GetWeatherForecastAsync()!;
+        var authState =
+            await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        Claims = authState.User.Claims.ToList();
+        var preferredUserNameCalim = authState.User.FindFirst("preferred_username");
+        if (preferredUserNameCalim is not null)
+        {
+            PreferredUserName = preferredUserNameCalim.Value;
+        }
+
+        Forecasts = await WeatherForecastService!.GetWeatherForecastAsync()!;
     }
 }
